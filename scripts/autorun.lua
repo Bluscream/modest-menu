05ef02854bd3c41ac2d9485d143066ded26e50c0
Bluscream <omitomit1@gmail.com>
2021-11-04 12:53:40 +0100

    fix autoruns.lua

--- scripts/autorun.lua
+++ scripts/autorun.lua
@@ -3,8 +3,9 @@ require "enums"
 local function OnPlayerChanged(oldPlayer, newPlayer)
 	if newPlayer == nil then return end
     newPlayer:set_config_flag(PedConfigFlag.DisableStoppingVehEngine, true)
-    newPlayer.set_config_flag(PedConfigFlag.DisableShufflingToDriverSeat, true)
+    newPlayer:set_config_flag(PedConfigFlag.DisableShufflingToDriverSeat, true)
     newPlayer:set_config_flag(PedConfigFlag.CanAttackFriendlies, true)
+    newPlayer:set_config_flag(PedConfigFlag.DiesByRagdoll, false)
 end
 
 menu.register_callback('OnPlayerChanged', OnPlayerChanged)
