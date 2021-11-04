c595cf5e3b323040436970b8cb3828c09d5b4438
Bluscream <omitomit1@gmail.com>
2021-11-04 17:47:51 +0100

    changes

--- scripts/autorun.lua
+++ scripts/autorun.lua
@@ -4,11 +4,12 @@ local function OnPlayerChanged(oldPlayer, newPlayer)
 	if newPlayer == nil then return end
     if firstRun then
         firstRun = false
-        -- if newPlayer.set_config_flag == nil then return end
-        newPlayer:set_config_flag(PedConfigFlag.DisableStoppingVehEngine, true)
-        newPlayer:set_config_flag(PedConfigFlag.DisableShufflingToDriverSeat, true)
-        newPlayer:set_config_flag(PedConfigFlag.CanAttackFriendlies, true)
-        newPlayer:set_config_flag(PedConfigFlag.DiesByRagdoll, false)
+        if newPlayer.set_config_flag ~= nil then
+            newPlayer:set_config_flag(PedConfigFlag.DisableStoppingVehEngine, true)
+            newPlayer:set_config_flag(PedConfigFlag.DisableShufflingToDriverSeat, true)
+            newPlayer:set_config_flag(PedConfigFlag.CanAttackFriendlies, true)
+            newPlayer:set_config_flag(PedConfigFlag.DiesByRagdoll, false)
+        end
         menu:detach_objects()
     end
 end
