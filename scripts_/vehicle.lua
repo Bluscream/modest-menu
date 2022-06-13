1b419ba0f45f64db7e55b91a54235af15e1b88f5
Bluscream <Bluscream@users.noreply.github.com>
2022-06-13 23:46:32 +0200

    idfk

--- scripts_/vehicle.lua
+++ scripts_/vehicle.lua
@@ -1,11 +1,35 @@
+local autoVehicleGodMode = true
+local autoRepairVehicle = true
+local autoVehicleLicensePlate = false
+-- local autoSiren = false
+
 local function OnVehicleChanged(oldVehicle, newVehicle)
 	if newVehicle ~= nil then
-		if newVehicle:get_model_hash() == 0x586765fb then
-			newVehicle:set_number_plate_text('outatime')
+		if autoRepairVehicle == true and newVehicle:get_health() < 1000 then
+			newVehicle:set_health(1000)
+			menu:level_current_vehicle()
 		end
-	
-		-- newVehicle:set_health(1000)
+		if autoVehicleGodMode == true and not newVehicle:get_godmode() then
+			-- menu.heal_vehicle()
+			newVehicle:set_godmode(true)
+		end
+		if autoVehicleLicensePlate == true then
+			newVehicleHash = newVehicle:get_model_hash()
+			if newVehicleHash == 0x586765fb then -- Deluxo
+				newVehicle:set_number_plate_text('outatime')
+			else
+				newVehicle:set_number_plate_text('x blu x')
+			end
+		end
+		-- if autoSiren == true and not newVehicle:get_config_flag(VehicleConfigFlag.SirenActive) then
+			-- newVehicle:set_config_flag(VehicleConfigFlag.PressingHorn, true)
+			-- newVehicle:set_config_flag(VehicleConfigFlag.SirenActive, true)
+		-- end
 	end
 end
 
 menu.register_callback('OnVehicleChanged', OnVehicleChanged)
+menu.add_toggle("Auto Vehicle Godmode", function() return autoVehicleGodMode end, function(v) autoVehicleGodMode = v end)
+menu.add_toggle("Auto Vehicle Repair", function() return autoRepairVehicle end, function(v) autoRepairVehicle = v end)
+menu.add_toggle("Auto Vehicle License Plate", function() return autoVehicleLicensePlate end, function(v) autoVehicleLicensePlate = v end)
+-- menu.add_toggle("Auto Siren", function() return autoSiren end, function(v) autoSiren = v end)
