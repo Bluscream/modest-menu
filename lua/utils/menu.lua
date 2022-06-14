d75c5ce7c1ed538e3a6b8e9f7f97638ced571977
Bluscream <Bluscream@users.noreply.github.com>
2022-06-14 17:53:12 +0200

    remove debugging stuffs

--- lua/utils/menu.lua
+++ lua/utils/menu.lua
@@ -1,5 +1,4 @@
 function menu_add_enum_range(enum, title, sort, action_callback, default_key_callback)
-    print("menu_add_enum_range 1")
     local enum_keys = table_get_keys(enum)
     local enum_keys_len = table_count(enum_keys)
     local enum_values = table_get_values(enum)
@@ -30,11 +29,6 @@ function menu_add_enum_range(enum, title, sort, action_callback, default_key_cal
         i = santize_index(table_index_of(enum_keys, curkey())-1)
         return set_title()
     end
-    -- local function get_key_from_title(title) return string.match(title, "| %w+") end
-    -- local function proxy_callback(title)
-    --     local key = get_key_from_title(title)
-    --     action_callback(key)
-    -- end
     menu.add_bare_item(title, set_title, function() action_callback(curval()) end, previous_item, next_item)
 end
 local function menu_centered_text(str)
