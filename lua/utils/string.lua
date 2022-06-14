a4c187773cbc4a1217b0c39b49a497214e464f13
Bluscream <Bluscream@users.noreply.github.com>
2022-06-15 00:02:53 +0200

    s

--- lua/utils/string.lua
+++ lua/utils/string.lua
@@ -1,3 +1,4 @@
 function string.startsWith(String,Start)
+    if String == nil or Start == nil then return false end
     return string.sub(String,1,string.len(Start))==Start
  end
--- scripts/vehicle.lua
+++ scripts/vehicle.lua
@@ -6,7 +6,30 @@ local autoVehicleLicensePlate = true
 local autoVehicleLock = false
 local autoSiren = false
 
+local function vehInfos(veh)
+	if not veh then return end
+	local hash = veh:get_model_hash()
+	local name = VehicleName[hash]
+	local licensePlate = veh:get_number_plate_text()
+	local godmode = veh:get_godmode()
+	local locked = veh:get_door_lock_state()
+	return hash, name, licensePlate, godmode, locked
+end
+
+local function vehString(veh, hash, name, licensePlate, godmode, locked)
+	str = name or tostring(veh)
+	if hash then str = str .. " (" .. hash .. ")" end
+	if licensePlate then str = str .. " [" .. licensePlate .. "]" end
+	if godmode then str = str .. " * " end
+	if locked ~= nil then str = str .. " " .. (locked and "LOCKED" or "UNLOCKED")..": "..tostring(locked) end
+	return dump(str)
+end
+
 local function OnVehicleChanged(oldVehicle, newVehicle)
+	local oldHash, oldName, oldLicensePlate, oldGodmode, oldLocked = vehInfos(oldVehicle)
+	local newHash, newName, newLicensePlate, newGodmode, newLocked = vehInfos(newVehicle)
+	newVeh = vehInfos(newVehicle)
+	log("OnVehicleChanged: " .. vehString(oldVehicle, oldHash, oldName, oldLicensePlate, oldGodmode, oldLocked) .. " | " .. vehString(newVehicle, newHash, newName, newLicensePlate, newGodmode, newLocked))
 	if newVehicle ~= nil then
 		if autoRepairVehicle == true and newVehicle:get_health() < 1000 then
 			newVehicle:set_health(1000)
@@ -17,10 +40,10 @@ local function OnVehicleChanged(oldVehicle, newVehicle)
 			newVehicle:set_godmode(true)
 			newVehicle:set_can_be_visibly_damaged(false)
 			newVehicle:set_window_collisions_disabled(true)
-			newVehicle:set_bouyance(1)
+			-- newVehicle:set_bouyance(0)
 			newVehicle:set_dirt_level(15)
-			newVehicle:set_max_speed(500)
-			newVehicle:set_window_tint(0)
+			-- newVehicle:set_max_speed(2)
+			newVehicle:set_window_tint(15)
 		end
 		if autoVehicleLicensePlate == true then
 			newVehicleHash = newVehicle:get_model_hash()
